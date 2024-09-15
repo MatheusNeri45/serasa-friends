1abd841f750d2fc1533c4193d994ca3267b8e66d
MatheusNeri45 <matheusazemneri@gmail.com>
2024-09-15 13:47:42 -0300

    Expenses created with proper split

--- src/app/api/createExpense/route.ts
+++ src/app/api/createExpense/route.ts
@@ -6,14 +6,36 @@ const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
   const req = await request.json();
+  let sumSplits = 0
+  const fullValue = req.value
+  for (let i = 0; i < req.debtors.length; i++){
+    sumSplits+= req.debtors[i].value
+  }
+  if(sumSplits!=fullValue){
+    return NextResponse.json(
+      { message: "You need to split the expense correctly."},
+      { status: 400 }
+    );
+  }
+
   try {
     const expense = await prisma.expense.create({
       data: {
         description: req.description,
-        value: req.value,
-        userId: req.userId,
-        groupId: req.groupId,
-        debtors: req.debtors
+        value: fullValue,
+        paidBy: {
+          connect: { id: req.userId }, 
+        },
+        debtors: {
+          createMany: {
+            data: req.debtors.map(
+              (debtor: { id: number; value: number }) => ({
+                participantId:debtor.id,
+                value: debtor.value,
+              })
+            ),
+          },
+        },
       },
     });
     return NextResponse.json(
@@ -21,6 +43,7 @@ export async function POST(request: NextRequest) {
       { status: 200 }
     );
   } catch (error) {
+    console.error("", error);
     return NextResponse.json(
       { message: "Unable to create expense" },
       { status: 500 }
