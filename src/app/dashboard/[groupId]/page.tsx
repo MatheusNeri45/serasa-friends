7e35182d3ac7d21bcfdd3ca511242f31c993115b
MatheusNeri45 <matheusazemneri@gmail.com>
2024-12-04 15:19:57 -0300

    fixed groupPage

--- src/app/dashboard/[groupId]/page.tsx
+++ src/app/dashboard/[groupId]/page.tsx
@@ -1,6 +1,6 @@
 import GroupPageClient from "@/app/components/dashboard/group-page";
 
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function GroupPage({ params }: { params: { groupId: string } }) {
-  return <GroupPageClient groupId={params.groupId} />;
+  return <GroupPageClient />;
 }
